5a173e72f53510654eec03d846dca84460acfdcb
Dmitriy B <kupperfeet@gmail.com>
2022-08-18 12:59:28 +0300

    task 10 done

---
+++ task10/Program.cs
@@ -0,0 +1,13 @@
+﻿// Показать кубы чисел, заканчивающихся на четную цифру
+Console.Write("Введите число: ");
+int UserNumber = int.Parse(Console.ReadLine());
+int OstUserNumber = UserNumber%10;
+if (OstUserNumber%2==0)
+{
+    int result = UserNumber*UserNumber*UserNumber;
+    Console.WriteLine("куб числа " +UserNumber+ " равно "+result);
+}
+else
+{
+    Console.WriteLine("Число заканчивается на нечётное число !");
+}
