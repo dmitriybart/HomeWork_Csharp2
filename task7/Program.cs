ad4e2e108422b80131235539508e9274820af53a
Dmitriy B <kupperfeet@gmail.com>
2022-08-18 12:12:04 +0300

    task 7 done modifide

--- task7/Program.cs
+++ task7/Program.cs
@@ -1,37 +1,11 @@
 ﻿// Определить количество цифр в числе
 Console.Write("Введите положительное число: ");
 int UserNumber = int.Parse(Console.ReadLine());
-if (UserNumber<100000000)
+int count = 0;
+int UserSecondNumber = UserNumber;
+while (UserSecondNumber>0)
 {
-if (UserNumber>=0 && UserNumber<=9)
-{
-    Console.WriteLine("Число " +UserNumber+ " - однозначное !");
-}if (UserNumber>=10 && UserNumber<=99)
-{
-    Console.WriteLine("Число " +UserNumber+ " - двухзначное !");
+    UserSecondNumber=UserSecondNumber/10;
+    count++;
 }
-if (UserNumber>=100 && UserNumber<=999)
-{
-    Console.WriteLine("Число " +UserNumber+ " - трехзначное !");
-}
-if (UserNumber>=1000 && UserNumber<=9999)
-{
-    Console.WriteLine("Число " +UserNumber+ " - четырехзначное !");
-}
-if (UserNumber>=10000 && UserNumber<=99999)
-{
-    Console.WriteLine("Число " +UserNumber+ " - пятизначное !");
-}
-if (UserNumber>=100000 && UserNumber<=999999)
-{
-    Console.WriteLine("Число " +UserNumber+ " - шестизначное !");
-}
-if (UserNumber>=1000000 && UserNumber<=9999999)
-{
-    Console.WriteLine("Число " +UserNumber+ " - семизначное !");
-}
-}
-else 
-{
-    Console.WriteLine("Введите подалуйста число по-меньше))");
-}
+Console.WriteLine("Количество цифр в числе " +UserNumber+ " равно " +count);
