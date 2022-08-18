6dea4b602e45df85ef7c728f3230a561f39a7115
Dmitriy B <kupperfeet@gmail.com>
2022-08-18 12:40:32 +0300

    task 9 done

---
+++ task9/Program.cs
@@ -0,0 +1,13 @@
+﻿// Написать программу вычисления произведения чисел от 1 до N
+Console.Write("Введите число: ");
+int UserNumber = int.Parse(Console.ReadLine());
+int sum = 1;
+int UserSecondNumber = UserNumber;
+int UserThreeNumber = 0;
+while (UserSecondNumber>0)
+{
+    UserThreeNumber=UserSecondNumber%10;
+    sum=sum*UserThreeNumber;
+    UserSecondNumber=UserSecondNumber/10;
+}
+Console.WriteLine("Сумма цифр в числе " +UserNumber+ " равно " +sum);
